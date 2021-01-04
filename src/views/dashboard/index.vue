89fa6cffecc8d91cc7b90c51be743b2b620ab415
jerry_sp <731238433@qq.com>
2021-01-04 18:23:18 +0800

    实现实时获取CPU信息

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,18 +1,78 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }}</div>
+    <div id="cpuChart" style="width: 250px;height: 250px" />
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
-
+import * as echarts from 'echarts'
+require('echarts/theme/macarons')
 export default {
   name: 'Dashboard',
+  data() {
+    return {
+      chart: null,
+
+      data: {
+        cpu: {}
+      }
+    }
+  },
   computed: {
     ...mapGetters([
       'name'
     ])
+  },
+  mounted() {
+    const that = this
+    const ws = new WebSocket('ws://127.0.0.1:7002/ws')
+    ws.addEventListener('message', ({ data }) => {
+      ws.send('hello')
+      that.data = JSON.parse(data)
+      that.setOptions(that.data)
+    })
+
+    this.$nextTick(() => {
+      this.initChart()
+    })
+  },
+  methods: {
+    initChart() {
+      this.chart = echarts.init(document.getElementById('cpuChart'))
+    },
+    setOptions({ cpu }) {
+      this.chart.setOption({
+        tooltip: {
+          formatter: '{a} <br/>{b} : {c}%'
+        },
+        series: [
+          {
+            name: '业务指标',
+            type: 'gauge',
+            detail: { formatter: `${cpu.used}%` },
+            axisLine: {
+              lineStyle: {
+                width: 10
+              }
+            },
+            splitLine: {
+              length: 15,
+              lineStyle: {
+                color: '#DCDFE6'
+              }
+            },
+            pointer: {
+              length: '75%',
+              width: 5
+            },
+            data: [
+              { value: cpu.used, name: 'CPU占用率' }
+            ]
+          }
+        ]
+      }, true)
+    }
   }
 }
 </script>
